ee601a4b84b3c6fa6bc58df66c3044b95822e1c3
Jim Gryck <halo2382001@yahoo.com>
2022-04-14 18:23:31 -0400

    added '' and null to trigger an alert, if correct characters added, prompt moving forward will show

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,23 @@ function generatePassword() {
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var number = "0123456789";
   var symbol = "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
-};
 
+
+  // create the length of characters for password
+  howManyCharacters = window.prompt("How many characters do you want in your password? Choose between 8-128 characters");
+    if (howManyCharacters < 8 || howManyCharacters > 128) {
+      window.alert("You must choose between 8-128 characters!");
+    }  
+    if (howManyCharacters === "" || howManyCharacters === null) {
+      window.alert("Please enter a valid number.");
+      // use return to call it again and stop the rest of this function from running
+      return howManyCharacters()
+    }
+    else {
+      window.alert("Your password will be " + howManyCharacters + " characters long.");
+    }
+    
+};    
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,7 +30,6 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
