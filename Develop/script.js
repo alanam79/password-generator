e19e6f4dd982a082083dc38f046b2c969824f68c
Jim Gryck <halo2382001@yahoo.com>
2022-04-15 11:37:16 -0400

    add password criteria questions

--- Develop/script.js
+++ Develop/script.js
@@ -10,18 +10,51 @@ function generatePassword() {
   howManyCharacters = window.prompt("How many characters do you want in your password? Choose between 8-128 characters");
     if (howManyCharacters < 8 || howManyCharacters > 128) {
       alert("You must choose between 8-128 characters!");
-      return howManyCharacters()
+      return howManyCharacters();
     }  
     if (howManyCharacters === "" || howManyCharacters === null) {
       alert("Please enter a valid number.");
       // use return to call it again and stop the rest of this function from running
-      return howManyCharacters()
+      return howManyCharacters();
     }
     else {
       alert("Your password will be " + howManyCharacters + " characters long.");
     }
-    
+  useLowerCase = confirm("Do you want to use lowercase characters?");
+    if (useLowerCase) {
+      alert("Your password will have lowercase characters.");
+    }    
+    else {
+      alert("Your password will not have lowercase characters.");
+    }
+  useUpperCase = confirm("Do you want to use uppercase characters?");
+    if (useUpperCase) {
+      alert("Your password will have uppercase characters");
+    }
+    else {
+      alert("your password will not have uppercase characters.");
+    }
+  useNumber = confirm("Do you want to use a number?");
+    if (useNumber) {
+      alert("Your password will have a number.");
+    }
+    else {
+      alert("Your password will not have a number.");
+    }
+  useSymbol = confirm("Do you want to use a symbol?");
+    if (useSymbol) {
+      alert("Your password will have a symbol.");
+    }
+    else {
+      alert("Your password will not have a symbol.");
+    }
+    if (useLowerCase === false && useUpperCase === false && useNumber === false && useSymbol === false) {
+      window.alert("Please select at least one lowercase, uppercase, number, and symbol to proceed.");
+    }  
 };    
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
